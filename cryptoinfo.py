66104200da37e9791a6a939dd21c54aa85e21056
Zach <Zach@ZACHSPC>
2018-09-19 17:43:53 +0100

    Updated Code and fixed typo's

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -3,9 +3,11 @@ def price(ctx):
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/' + ctx)
     data = r.json()
     price = data[0]['price_usd']
-    print('$' + price)
-price("bitcoin")
+    rank = data[0]['rank']
+    max_supply = data[0]['total_supply'] 
+    print('Coin: ' + ctx + "\n" + 'Price: $' + price + "\n" + 'Rank: ' + rank + "\n" + "Max Supply: " + max_supply)
 
-#edit
+
+
 
 
