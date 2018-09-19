4f7ec85953e26587215e09aaf05bd5366a6bb97b
Zach <Zach@ZACHSPC>
2018-09-19 17:35:52 +0100

    Typo fixed

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -1,11 +1,11 @@
-mport requests
+import requests
 def price(ctx):
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/' + ctx)
     data = r.json()
     price = data[0]['price_usd']
     print('$' + price)
+price("bitcoin")
 
-
-
+#edit
 
 
